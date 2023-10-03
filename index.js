bc678727bca6d1535bc83fe6a293be488126600e
biandito <bekatrivz@gmail.com>
2023-10-03 20:13:26 -0300

    update +add bodyparser

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
-const app = require('express') ();
-const PORT = 800;
+import express from 'express'
+import bodyParser from 'body-parser';
 
+const app = express();
+const PORT = 5000;
+
+app.use(bodyParser.json());
+
+app.get('/',(req,res) => { 
+    console.log('[TEST]')
+    res.send('Hello')
+ }) 
+
+app.listen(PORT,() => console.log(`Servidor no PORT: http://localhost:${PORT}`));
+
+// USUARIOS //
+
+// app.post('/users/signup')
+
+// app.post('/users/login')
+
+// app.put('/users/:id')
+
+// app.delete('/users/:id')
+
+// // ADMS //
+
+//  app.post('/admin/login')
+//  // add //
+
+//  app.get('/admin/reports')
+// // add //
+
+// app.get('/admin/users')
+// // add //
+
+// /** ITENS */
